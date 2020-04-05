404902636f993bb4685ed9cbdead7887005afc12
12378 <123781007@qq.com>
2020-04-05 12:25:23 +0800

    第一次的实验

--- src/main/java/org/csu/mypetstore/controller/AccountController.java
+++ src/main/java/org/csu/mypetstore/controller/AccountController.java
@@ -1,5 +1,6 @@
 package org.csu.mypetstore.controller;
 
+
 import org.csu.mypetstore.domain.Account;
 import org.csu.mypetstore.domain.Product;
 import org.csu.mypetstore.service.AccountService;
@@ -8,12 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
 @RequestMapping("/account/")
-@SessionAttributes({"account","myList","authenticated"})
+@SessionAttributes({"account", "myList", "authenticated"})
 public class AccountController {
 
     @Autowired
@@ -22,53 +24,95 @@ public class AccountController {
     @Autowired
     private CatalogService catalogService;
 
+    private static final List<String> LANGUAGE_LIST;
+    private static final List<String> CATEGORY_LIST;
+
+    static {
+        List<String> langList = new ArrayList<String>();
+        langList.add("ENGLISH");
+        langList.add("CHINESE");
+        LANGUAGE_LIST = Collections.unmodifiableList(langList);
+
+        List<String> catList = new ArrayList<String>();
+        catList.add("FISH");
+        catList.add("DOGS");
+        catList.add("REPTILES");
+        catList.add("CATS");
+        catList.add("BIRDS");
+
+        CATEGORY_LIST = Collections.unmodifiableList(catList);
+    }
+
     @GetMapping("signonForm")
-    public String signonForm(){
+    public String signonForm() {
         return "account/signon";
     }
 
     @PostMapping("signon")
-    public String signon(String username, String password, Model model){
-        Account loginAccount = accountService.getAccount(username,password);
+    public String signon(String username, String password, Model model) {
+        Account account = accountService.getAccount(username, password);
 
-        if(loginAccount == null){
+        if (account == null) {
             String msg = "Invalid username or password.  Signon failed.";
-            model.addAttribute("msg",msg);
+            model.addAttribute("msg", msg);
             return "account/signon";
-        }else {
-            loginAccount.setPassword(null);
-            List<Product> myList =catalogService.getProductListByCategory(loginAccount.getFavouriteCategoryId());
+        } else {
+            account.setPassword(null);
+            List<Product> myList = catalogService.getProductListByCategory(account.getFavouriteCategoryId());
             boolean authenticated = true;
-            model.addAttribute("account", loginAccount);
-            model.addAttribute("myList",myList);
-            model.addAttribute("authenticated",authenticated);
+            model.addAttribute("account", account);
+            model.addAttribute("myList", myList);
+            model.addAttribute("authenticated", authenticated);
             return "catalog/main";
         }
     }
-    @GetMapping("registerPage")
-    public String registerPage()
-    {
-        return "account/NewAccountForm";
-    }
-    @PostMapping("register")
-    public String register(String userId,String password,String firstName,String lastName,String email,String phone,String addr1,String addr2,String city,String state,String zip,String country,Model model)
-    {
-        Account account = new Account();
-        account.setUsername(userId);
-        account.setPassword(password);
-        account.setFirstName(firstName);
-        account.setLastName(lastName);
-        account.setEmail(email);
-        account.setPhone(phone);
-        account.setAddress1(addr1);
-        account.setAddress2(addr2);
-        account.setCity(city);
-        account.setState(state);
-        account.setZip(zip);
-        account.setCountry(country);
-        System.out.println(account.toString());
-        accountService.insertAccount(account);
+
+    @GetMapping("signoff")
+    public String signoff(Model model) {
+        Account loginAccount = new Account();
+        List<Product> myList = null;
+        boolean authenticated = false;
+        model.addAttribute("account", loginAccount);
+        model.addAttribute("myList", myList);
+        model.addAttribute("authenticated", authenticated);
         return "catalog/main";
     }
 
+    @GetMapping("editAccountForm")
+    public String editAccountForm(@SessionAttribute("account") Account account , Model model) {
+        model.addAttribute("account", account);
+        model.addAttribute("LANGUAGE_LIST", LANGUAGE_LIST);
+        model.addAttribute("CATEGORY_LIST", CATEGORY_LIST);
+        return "account/edit_account";
+    }
+
+    @PostMapping("editAccount")
+    public String editAccount(Account account, String repeatedPassword, Model model) {
+        if (account.getPassword() == null || account.getPassword().length() == 0 || repeatedPassword == null || repeatedPassword.length() == 0) {
+            String msg = "密码不能为空";
+            model.addAttribute("msg", msg);
+            return "account/edit_account";
+        } else if (!account.getPassword().equals(repeatedPassword)) {
+            String msg = "两次密码不一致";
+            model.addAttribute("msg", msg);
+            return "account/edit_account";
+        } else {
+            accountService.updateAccount(account);
+            account = accountService.getAccount(account.getUsername());
+            List<Product> myList = catalogService.getProductListByCategory(account.getFavouriteCategoryId());
+            boolean authenticated = true;
+            model.addAttribute("account", account);
+            model.addAttribute("myList", myList);
+            model.addAttribute("authenticated", authenticated);
+            return "redirect:/catalog/index";
+        }
+    }
+
+    @GetMapping("newAccountForm")
+    public String newAccountForm(Model model){
+        model.addAttribute("newAccount",new Account());
+        model.addAttribute("LANGUAGE_LIST", LANGUAGE_LIST);
+        model.addAttribute("CATEGORY_LIST", CATEGORY_LIST);
+        return "account/new_account";
+    }
 }
