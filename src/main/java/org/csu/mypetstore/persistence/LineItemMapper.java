ddf885aac69d7fc399379f0582638eaeb305ac95
12378 <123781007@qq.com>
2020-04-07 15:06:27 +0800

    在订单中一直挣扎

--- src/main/java/org/csu/mypetstore/persistence/LineItemMapper.java
+++
@@ -1,9 +0,0 @@
-package org.csu.mypetstore.persistence;
-
-import org.csu.mypetstore.domain.LineItem;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LineItemMapper {
-    LineItem getLineItem(int orderId);
-}
